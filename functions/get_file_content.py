67b2ebd78e30893e5a396b49ff957b2853cb0e9d
M-Sviridov <74146348+M-Sviridov@users.noreply.github.com>
2025-12-06 19:11:29 +0700

    Add function to list content of a file

---
+++ functions/get_file_content.py
@@ -0,0 +1,20 @@
+import os
+
+MAX_CHARS = 10000
+
+def get_file_content(working_directory, file_path):
+    full_path = os.path.join(working_directory, file_path)
+    abs_full = os.path.abspath(full_path)
+    abs_work = os.path.abspath(working_directory)
+
+    if not abs_full.startswith(abs_work):
+        return print(f'Error: Cannot read "{file_path}" as it is outside the permitted working directory')
+
+    if not os.path.isfile(full_path):
+        return print(f'Error: File not found or is not a regular file: "{file_path}"')
+
+    with open(full_path) as f:
+        content = f.read()
+        if len(content) > MAX_CHARS:
+            content = content[:10000] + f'[...File "{file_path}" truncated at 10000 characters]'
+        return print(content)
