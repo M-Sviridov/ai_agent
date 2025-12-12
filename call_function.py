f3a8161a7a9444808f8d651fbe6b9b92653aff88
M-Sviridov <74146348+M-Sviridov@users.noreply.github.com>
2025-12-12 20:15:57 +0700

    Add call_function.py but it does not fulfill the boot.dev checks yet

---
+++ call_function.py
@@ -0,0 +1,52 @@
+from functions.get_file_content import get_file_content, schema_get_file_content
+from functions.get_files_info import get_files_info, schema_get_files_info
+from functions.run_python_file import run_python_file, schema_run_python_file
+from functions.write_file import write_file, schema_write_file
+from google.genai import types
+
+available_functions = types.Tool(
+    function_declarations=[
+        schema_get_files_info,
+        schema_get_file_content,
+        schema_run_python_file,
+        schema_write_file,
+    ],
+)
+
+def call_function(function_call_part, verbose=False):
+    function_name = function_call_part.name
+    function_args = dict(function_call_part.args)
+    function_args["working_directory"] = "./calculator"
+
+    if verbose:
+        print(f"Calling function: {function_name}({function_args})")
+    else:
+        print(f" - Calling function: {function_name}")
+    valid_functions = {
+            "get_file_content": get_file_content,
+            "get_files_info": get_files_info,
+            "run_python_file": run_python_file,
+            "write_file": write_file,
+            }
+    if function_name not in valid_functions:
+        return types.Content(
+            role="tool",
+            parts=[
+                types.Part.from_function_response(
+                    name=function_name,
+                    response={"error": f"Unknown function: {function_name}"},
+                )
+            ],
+        )
+
+    function_result = valid_functions[function_name](**function_args)
+
+    return types.Content(
+        role="tool",
+        parts=[
+            types.Part.from_function_response(
+                name=function_name,
+                response={"result": function_result},
+            )
+        ],
+    )
